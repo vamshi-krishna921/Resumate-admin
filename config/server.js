44caa910bcd2043ad9614520a4e45f97f9b67fb9
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-02 21:57:41 +0530

    Everthing is resumed

--- config/server.js
+++ config/server.js
@@ -1,7 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: parseInt(process.env.PORT, 10),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
