fddf468d6d6f942814280e9cc08e19fa254abae6
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-02 21:01:38 +0530

    Strapi errors fixed

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: parseInt(process.env.PORT, 10),
+  host: env("HOST", "0.0.0.0"), // Required by Render
+  port: Number(process.env.PORT) || 1337, // Use Render's injected PORT
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array("APP_KEYS"), // Strapi app keys
   },
   webhooks: {
     populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
