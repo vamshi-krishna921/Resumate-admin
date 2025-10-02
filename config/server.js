585df04b04d353d594362fb722149c804abb20ae
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-02 20:39:27 +0530

    Backend errors fixed

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: process.env.PORT || 1337, // Use Render's port directly
   app: {
     keys: env.array("APP_KEYS"),
   },
