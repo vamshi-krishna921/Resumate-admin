2ec3fbe6786986c4a213d65eed117e1226933dc2
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-02 19:22:53 +0530

    Backend

--- config/database.js
+++ config/database.js
@@ -1,25 +1,18 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: "mysql",
+    client: 'mysql',
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", ""),
-      ssl: env.bool("DATABASE_SSL", false)
-        ? {
-            rejectUnauthorized: env.bool(
-              "DATABASE_SSL_REJECT_UNAUTHORIZED",
-              false
-            ),
-          }
-        : false,
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: env.bool('DATABASE_SSL', false),
     },
     pool: {
-      min: env.int("DATABASE_POOL_MIN", 2),
-      max: env.int("DATABASE_POOL_MAX", 10),
+      min: env.int('DATABASE_POOL_MIN', 2),
+      max: env.int('DATABASE_POOL_MAX', 10),
     },
-    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+    acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
   },
 });
