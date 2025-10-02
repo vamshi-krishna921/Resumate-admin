548df24f37ad4bb57da6743d7fea0e604d9a913a
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-02 21:24:31 +0530

    Fixed errors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,20 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [
+        "https://resumate-admin-757h.onrender.com",
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/server.js
+++ config/server.js
@@ -1,10 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"), // Required by Render
-  port: Number(process.env.PORT) || 1337, // Use Render's injected PORT
+  host: env("HOST", "0.0.0.0"),
+  port: parseInt(process.env.PORT, 10) || 1337,
   app: {
-    keys: env.array("APP_KEYS"), // Strapi app keys
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    keys: env.array("APP_KEYS"),
   },
 });
