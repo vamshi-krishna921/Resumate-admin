ed7508e44d04ab86d1a51fcbd3cd81f5f09808f0
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-03 06:54:02 +0530

    logs

--- config/database.js
+++ config/database.js
@@ -1,15 +1,25 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "mysql",
+module.exports = ({ env }) => {
+  const client = env("DATABASE_CLIENT", "mysql");
+  console.log("Trying to connect to DB:", {
+    host: env("DATABASE_HOST"),
+    port: env.int("DATABASE_PORT"),
+    database: env("DATABASE_NAME"),
+    user: env("DATABASE_USERNAME"),
+  });
+
+  return {
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
-      ssl: env.bool("DATABASE_SSL", false),
+      client,
+      connection: {
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        user: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool("DATABASE_SSL", false),
+      },
+      pool: { min: 2, max: 10 },
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
-    pool: { min: 2, max: 10 },
-    acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
-  },
-});
+  };
+};
