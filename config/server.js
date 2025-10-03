21dd3c651cc022b9912f5462dd8f12c010139f8f
vamshi-krishna921 <kallurivamshikrishna921@gmail.com>
2025-10-03 06:59:57 +0530

    Strapi backend

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', process.env.PORT || 1337),
+  host: "0.0.0.0",
+  port: parseInt(env("PORT", 1337), 10),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
